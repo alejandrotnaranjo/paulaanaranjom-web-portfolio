a340650b699063cf5c6adce45de0dd56c6c62cd3
Alejandro T. Naranjo <alejandrotnaranjo@proton.me>
2024-07-30 08:37:08 -0500

    Contact form

--- src/components/section/ContactSection.jsx
+++ src/components/section/ContactSection.jsx
@@ -1,23 +1,56 @@
-import { useRef } from "react";
+// import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 
-const ContactSection = () => {
-  const form = useRef();
+import { useState } from "react";
 
-  const sendEmail = (e) => {
+const ContactSection = () => {
+  // const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+  //   emailjs
+  //     .sendForm("service_nd0nnpo", "template_qqcgc9d", form.current, {
+  //       publicKey: "q5QD-bPiymGvhAh-N",
+  //     })
+  //     .then(
+  //       () => {
+  //         console.log("SUCCESS!");
+  //       },
+  //       (error) => {
+  //         console.log("FAILED...", error.text);
+  //       }
+  //     );
+  // };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    const serviceID = "service_kn5tqkn";
+    const templateID = "template_hkb5sr2";
+    const publicKey = "kyW3vcNOYIg0iX5Dx";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Paula Andrea Naranjo M.",
+      message: message,
+    };
+
     emailjs
-      .sendForm("service_nd0nnpo", "template_qqcgc9d", form.current, {
-        publicKey: "q5QD-bPiymGvhAh-N",
+      .send(serviceID, templateID, templateParams, publicKey)
+      .then((response) => {
+        alert("Correo enviado exitosamente.");
+        console.log("Email sent successfully", response);
+        setName("");
+        setEmail("");
+        setMessage("");
       })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
+      .catch((error) => {
+        alert("Error enviando el correo, por favor intentalo más tarde.");
+        console.log("Error sending email: ", error);
+      });
   };
 
   return (
@@ -25,17 +58,29 @@ const ContactSection = () => {
       <p id="section-list-title">Contacto</p>
       <div className="contact-form-section-container">
         <form
+          id="form__"
           className="contact-form-container"
-          ref={form}
-          onSubmit={sendEmail}
+          onSubmit={handleSubmit}
         >
-          <input disabled placeholder="Nombre" type="text" name="user_name" />
-          <input disabled placeholder="Correo" type="email" name="user_email" />
-          <input disabled placeholder="Asunto" type="text" name="" />
-          <input disabled placeholder="Mensaje" id="message" name="message" />
-          <button disabled type="submit">
-            Enviar
-          </button>
+          <input
+            placeholder="Nombre"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder="Correo"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Mensaje"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Enviar</button>
         </form>
       </div>
     </div>
